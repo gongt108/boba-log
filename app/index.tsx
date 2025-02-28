d6f11b1dd3f1b1bba35269a68b5113ea55f85835
Tiffany Gong <gongt108@gmail.com>
2025-02-27 22:37:16 -0800

    update README

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default function Index() {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Text>Did you get boba today?</Text>
+			<Button
+				icon="camera"
+				mode="contained"
+				onPress={() => console.log('Pressed')}
+			>
+				Add My Order
+			</Button>
+			<Button mode="contained" onPress={() => console.log('Pressed')}>
+				See Past Orders
+			</Button>
+		</View>
+	);
+}
