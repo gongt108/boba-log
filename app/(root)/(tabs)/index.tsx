7213924a5407083234e87836f88d0f7e03308208
Tiffany Gong <gongt108@gmail.com>
2025-03-06 23:44:38 -0800

    add react-native-vector-icons library

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,26 +1,30 @@
+import React, { useState } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import { Button } from 'react-native-paper';
+import { Button, Searchbar } from 'react-native-paper';
 
 export default function Index() {
+	const [searchQuery, setSearchQuery] = useState('');
+
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}
-		>
+		<View className="flex-1 p-4">
 			<Text>Did you get boba today?</Text>
-			<Button
-				icon="camera"
-				mode="contained"
-				onPress={() => console.log('Pressed')}
-			>
-				Add My Order
-			</Button>
-			<Button mode="contained" onPress={() => console.log('Pressed')}>
-				See Past Orders
-			</Button>
+
+			<Searchbar
+				placeholder="Search"
+				onChangeText={(query) => setSearchQuery(query)}
+				value={searchQuery}
+			/>
+
+			<View>
+				<View className="flex flex-row justify-between">
+					<Text>Favorites</Text>
+					<Text>See All</Text>
+				</View>
+				<View>
+					<Text>Past Stores</Text>
+					<Text>See All</Text>
+				</View>
+			</View>
 		</View>
 	);
 }
